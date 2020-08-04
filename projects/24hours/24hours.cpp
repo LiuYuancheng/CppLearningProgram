f10f4f060c0be9fd2fb67801ac134d90261c9489
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-08-04 16:26:49 +0800

    Added the Vscode compile tips.

--- projects/24hours/24hours.cpp
+++ projects/24hours/24hours.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+
+// In vscode select Terminal> Run build Task > C/C++:g++.exe build atice file to compile.
+
 int main(){
     std::cout << "Solidum petit in profundis. \n";
     return 0;
