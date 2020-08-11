abe0863f6ea7288e195d8d3a9a3a6148f9631284
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-08-11 17:39:12 +0800

    hour 2 code example test.

--- projects/24hours/24hours.cpp
+++ projects/24hours/24hours.cpp
@@ -3,8 +3,16 @@
 
 // In vscode select Terminal> Run build Task > C/C++:g++.exe build atice file to compile.
 
-int main(){
-    std::cout << "Solidum petit in profundis. \n";
-    return 0;
+int add(int x, int y)
+{
+    // add integer x and integer y then return the sum value
+    std::cout << "Calculating ... \n";
+    return x + y;
 }
 
+int main()
+{
+    std::cout << "section2: sum two int together: \n";
+    std::cout << " 123 + 456 =" << add(123,456) <<"\n";
+    return 0;
+}
