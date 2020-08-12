a5a3bd07581452a98d57466ca36775d1d8247f60
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-08-12 20:47:31 +0800

    added std i/o test.

--- projects/24hours/24hours.cpp
+++ projects/24hours/24hours.cpp
@@ -14,8 +14,23 @@ int add(int x, int y)
     return x + y;
 }
 
+void section2()
+{
+    // C++ stand input/output test.
+    std::cout << "Section2: sum two int together: \n";
+    int x, y;
+    std::cout << "Type in the 1st int:";
+    std::cin >> x;
+    std::cout << "Type in the 2nd int:";
+    std::cin >> y;
+    std::cout << "Result:\n" << add(x,y) <<"\n";
+    std::cout << "========================\n";
+}
+
+
 void section3()
 {
+    // C++ data type test.
     std::cout << "Section 3 Creating Variable and Constants \n";
     std::cout << " - The size of Integer : \t" << sizeof(int) << "bytes \n";
     std::cout << " - The size of short : \t\t" << sizeof(short) << "bytes \n";
@@ -23,19 +38,19 @@ void section3()
     std::cout << " - The size of char : \t\t" << sizeof(char) << "bytes \n";
     std::cout << " - The size of float : \t\t" << sizeof(float) << "bytes \n";
     std::cout << " - The size of double : \t" << sizeof(double) << "bytes \n";
-    std::cout << " - The size of long long int : \t" << sizeof(long long int) << "bytes \n";    
+    std::cout << " - The size of long long int : \t" << sizeof(long long int) << "bytes \n";
+    // C++ constant
+    std::cout << " Constant val 'RED' = \t" << RED <<"\n";
+    std::cout << " Constant val 'GREEN' = " << GREEN <<"\n";
+    std::cout << " Constant val 'BLUE' = \t" << BLUE <<"\n";
+    //
+    std::cout << "========================\n";
 }
 
 
 int main()
 {
-    std::cout << "section2: sum two int together: \n";
-    std::cout << " 123 + 456 =" << add(123,456) <<"\n";
+    section2();
     section3();
-    std::cout << " Constant val 'RED' = \t" << RED <<"\n";
-    std::cout << " Constant val 'GREEN' = \t" << GREEN <<"\n";
-    std::cout << " Constant val 'BLUE' = \t" << BLUE <<"\n";
-
-
     return 0;
 }
