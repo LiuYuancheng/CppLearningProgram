d3a427d09a1e6261b6836d716e84914bdfe741b9
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-08-12 10:36:09 +0800

    Added hour 3 test programs.

--- projects/24hours/24hours.cpp
+++ projects/24hours/24hours.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+//two way to define a constants:
+const int TEST_MD1 = true;
+#define TEST_MD2 true; 
 
+enum COLOR {RED, BLUE, GREEN}; // default re
 // In vscode select Terminal> Run build Task > C/C++:g++.exe build atice file to compile.
 
 int add(int x, int y)
@@ -10,9 +14,28 @@ int add(int x, int y)
     return x + y;
 }
 
+void section3()
+{
+    std::cout << "Section 3 Creating Variable and Constants \n";
+    std::cout << " - The size of Integer : \t" << sizeof(int) << "bytes \n";
+    std::cout << " - The size of short : \t\t" << sizeof(short) << "bytes \n";
+    std::cout << " - The size of long : \t\t" << sizeof(long) << "bytes \n";
+    std::cout << " - The size of char : \t\t" << sizeof(char) << "bytes \n";
+    std::cout << " - The size of float : \t\t" << sizeof(float) << "bytes \n";
+    std::cout << " - The size of double : \t" << sizeof(double) << "bytes \n";
+    std::cout << " - The size of long long int : \t" << sizeof(long long int) << "bytes \n";    
+}
+
+
 int main()
 {
     std::cout << "section2: sum two int together: \n";
     std::cout << " 123 + 456 =" << add(123,456) <<"\n";
+    section3();
+    std::cout << " Constant val 'RED' = \t" << RED <<"\n";
+    std::cout << " Constant val 'GREEN' = \t" << GREEN <<"\n";
+    std::cout << " Constant val 'BLUE' = \t" << BLUE <<"\n";
+
+
     return 0;
 }
