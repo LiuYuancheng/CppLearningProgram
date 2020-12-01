8f9117412f64edab37dc79c77b54a4ea11332c87
yc <liu_yuan_cheng@hotmail.com>
2020-12-01 20:21:50 +0800

    try to use the so in a normal cpp file.

--- projects/soLibTest/soTest.cpp
+++ projects/soLibTest/soTest.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include "solibrary.hpp"
 #include <iostream>
+#include <dlfcn.h>
+
+extern "C" typedef int (*print_value)( int x);
 
 int main(void){
-	std::cout <<print_value(10);
+  	void* lib = dlopen("./libhello.so", RTLD_LAZY);
+  	print_value hello = (print_value)dlsym( lib, "print_value" );
+
+  	hello(10);
+	dlclose(lib);
+	//std::cout << (*printVal)(10);
+	//dlclose(lib);
 	return 0;
 }
